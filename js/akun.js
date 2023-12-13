7c5e385634b41e57ccde3ff3a1a353224bf3863e
Cangcorangsz <bebekkeren1234@gmail.com>
2023-12-13 18:51:08 +0700

    +Menambahkan menu registrasi

--- js/akun.js
+++ js/akun.js
@@ -1,17 +1,32 @@
 const akunButton = document.getElementById("akunButton");
 const akun = document.getElementById("akun");
 const closeAkun = document.getElementById("closeAkun");
+const loginForm = document.querySelector(".akun-form");
+const registerForm = document.querySelector(".register-form");
+const register = document.getElementById("register");
+const loginDisini = document.getElementById("loginDisini");
+const mainContainer = document.getElementById("mainContainer");
 
 akunButton.addEventListener("click", function () {
     akun.style.display = "block";
 })
 
-closeAkun.addEventListener("click", function(){
+closeAkun.addEventListener("click", function () {
     akun.style.display = "none";
 })
 
-window.addEventListener("click", function(event) {
+window.addEventListener("click", function (event) {
     if (event.target == akun) {
         akun.style.display = "none";
     }
 })
+
+register.addEventListener("click", function () {
+    loginForm.style.display = "none";
+    registerForm.style.display = "block";
+});
+
+loginDisini.addEventListener("click", function() {
+    loginForm.style.display = "block";
+    registerForm.style.display = "none";
+});
